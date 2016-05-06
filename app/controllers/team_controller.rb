fc3495e539eb1783bbee53e44e62d7d67e3074df
msalad <msalad@uncc.edu>
2016-05-06 01:54:24 +0000

    Updated all teams but the Hornets

---
+++ app/controllers/team_controller.rb
@@ -0,0 +1,11 @@
+class TeamController < ApplicationController
+    def index
+    #@players = Player.all
+    #byebug
+    #@player = Player.create(player_params)
+    #byebug
+    #redirect_to home_index_path
+    
+    end
+
+end
